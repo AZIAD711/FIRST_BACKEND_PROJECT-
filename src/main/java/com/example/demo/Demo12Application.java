f95622009e3ce9859be68aeebaf952f1dc1857f6
AZIAD711 <aziad711@gmail.com>
2025-09-14 12:38:06 +0300

    Update Demo12Application.java

--- src/main/java/com/example/demo/Demo12Application.java
+++ src/main/java/com/example/demo/Demo12Application.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/api")
+// @RequestMapping("/api")
 public class Demo12Application {
 
     public static void main(String[] args) {
